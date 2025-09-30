b51fba999a199d26e38f28f942f4c5286b398704
Chinaglia <156460261+chinagliadev@users.noreply.github.com>
2025-09-30 19:24:28 -0300

    Inserindo a classe pessoa autorizada.
    Implementação de funções
    - cadastrarPessoasAutorizada

---
+++ class/PessoaAutorizada.php
@@ -0,0 +1,34 @@
+<?php
+
+class PessoaAutorizada
+{
+
+    private $conn;
+
+    public function __construct()
+    {
+        $dsn = "mysql:dbname={$_ENV['BANCO']};host={$_ENV['HOST']}";
+        $usuario = $_ENV['USUARIO'];
+        $senha = $_ENV['SENHA'];
+
+        $this->conn = new PDO($dsn, $usuario, $senha);
+    }
+
+    public function cadastrarPessoaAutorizada($nome, $cpf, $celular, $parentesco){
+
+        $sqlInserir = "INSERT INTO tb_pessoas_autorizadas (nome, cpf, celular, parentesco) VALUES (:nome, :cpf, :celular, :parentesco)";
+
+        $dados = $this->conn->prepare($sqlInserir);
+
+        $dados->execute([
+            ":nome" => $nome,
+            ":cpf" => $cpf,
+            ":celular" => $celular,
+            ":parentesco" => $parentesco
+        ]);
+
+
+        return $this->conn->lastInsertId();
+    }
+
+}
