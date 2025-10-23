29022b84b13b40d8312f26f49533ef0137360f29
Chinaglia <156460261+chinaglia77@users.noreply.github.com>
2025-10-23 17:00:17 -0300

    implementando o campo tipo de moradia - etapa estrurua famililar

--- template/cadastro_aluno/estrutura-familiar.php
+++ template/cadastro_aluno/estrutura-familiar.php
@@ -127,6 +127,42 @@
         </div>
     </div>
 
+    <h4 class="ui dividing header">Tipo de moradia</h4>
+    <div class="fields">
+        <div class="field">
+            <div class="ui radio checkbox">
+                <input type="radio" name="tipo_moradia" value="propria" id="moradia_propria">
+                <label for="moradia_propria">Própria</label>
+            </div>
+        </div>
+        <div class="field">
+            <div class="ui radio checkbox">
+                <input type="radio" name="tipo_moradia" value="cedida" id="moradia_cedida">
+                <label for="moradia_cedida">Cedida</label>
+            </div>
+        </div>
+        <div class="field">
+            <div class="ui radio checkbox">
+                <input type="radio" name="tipo_moradia" value="alugada" id="moradia_alugada">
+                <label for="moradia_alugada">Alugada</label>
+            </div>
+        </div>
+    </div>
+    <div class="field">
+        <div class="four wide field">
+            <label for="txtValorAluguel">Valor do aluguel</label>
+            <div class="ui small input">
+                <input name="txtValorAluguel" id="txtValorAluguel" type="number" placeholder="Valor do aluguel">
+            </div>
+            <div class="ui hidden negative message" id="mensagem-erro-cirurgia">
+                <div class="content">
+                    <i class="heartbeat icon"></i>
+                    <span id="cirurgia-erro"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <h4 class="ui dividing header">Transporte para a escola</h4>
     <div class="fields">
         <div class="field">
