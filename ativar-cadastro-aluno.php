232136817af114fdb215e8bc86cfeab3c996c9b4
Chinaglia <156460261+chinagliadev@users.noreply.github.com>
2025-10-29 09:43:09 -0300

    Ativando matricula do aluno - modal ativar matricula, classe matricula com a funcao reativarMatricula

--- ativar-cadastro-aluno.php
+++ ativar-cadastro-aluno.php
@@ -3,4 +3,17 @@
 require('./class/Matricula.php');
 require('./config.php');
 
+if($_SERVER['REQUEST_METHOD']=== 'POST'){
+    $id_aluno = $_POST['id_aluno'] ?? null;
+
+    if($id_aluno !== null){
+        $matricula = new Matricula();
+        
+        $matricula->reativarMatricula($id_aluno);
+        header('location: ./cadastrados.php');
+        var_dump($matricula);
+
+    }
+}
+
 ?>
