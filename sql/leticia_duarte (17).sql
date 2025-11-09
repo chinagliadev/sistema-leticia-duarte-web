7ae789f97ba3de83914e942037a88856e81de8d8
Chinaglia <156460261+chinaglia77@users.noreply.github.com>
2025-11-09 20:55:51 -0300

    inserindo o banco de dados

--- sql/leticia_duarte (17).sql
+++ sql/leticia_duarte (17).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27/10/2025 às 12:40
+-- Tempo de geração: 10/11/2025 às 00:53
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.0.30
 
